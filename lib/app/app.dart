934f6741164b795cf75abfc66ab76d8d69076716
mr-riad <riadgangni01302@gmail.com>
2025-09-04 09:33:54 +0600

    initialization firebase core

---
+++ lib/app/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
