25dd18917f1f8270d0f3e873d3c218e2b4be8b11
mr-riad <riadgangni01302@gmail.com>
2025-09-04 12:31:31 +0600

    use fvm version 3.32.2 & initialization crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 
+import 'dart:ui';
+
 import 'package:ecommerce_project1/app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -10,5 +13,14 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(CraftyBay());
+
 }
